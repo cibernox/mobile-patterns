f1e55b8b64977b0a89ff4bc515dbcfd3f032bf1d
Miguel Camba <miguel.camba@gmail.com>
2015-01-03 01:33:25 +0000

    Add previous/next relations between articles
    Conflicts:
    	app/templates/news/show.hbs

--- app/models/article.js
+++ app/models/article.js
@@ -4,4 +4,6 @@ export default DS.Model.extend({
   header: DS.attr('string'),
   body: DS.attr('string'),
   thumbnailUrl: DS.attr('string'),
+  previousArticle: DS.belongsTo('article', { inverse: 'nextArticle' }),
+  nextArticle: DS.belongsTo('article', { inverse: 'previousArticle' })
 });
