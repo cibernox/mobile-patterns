ada2cef713e085d9d5187d3a567e55941380b004
Miguel Camba <miguel.camba@gmail.com>
2015-01-04 00:38:06 +0000

    Animation finalizes on touchend event

--- app/views/news/show.js
+++ app/views/news/show.js
@@ -3,6 +3,7 @@ import Gesture from 'mobile-patterns/utils/gesture';
 
 export default Ember.View.extend({
   progress: 0, // Progress varies between -1 and +1
+  defaultSpeed: 0.045,
   ignoreGesture: false,
   trackGesture: false,
 
@@ -18,13 +19,13 @@ export default Ember.View.extend({
 
   touchMove: function(evt) {
     if (!this.ignoreGesture) {
-      evt.preventDefault();
       this.gesture.push(evt.originalEvent);
       this.handleGesture();
     }
   },
 
   touchEnd: function() {
+    this.finishAnimation();
     this.ignoreGesture = false;
     this.trackGesture = false;
   },
@@ -35,24 +36,56 @@ export default Ember.View.extend({
       return;
     }
 
-    if (this.trackGesture || Math.abs(this.gesture.deltaX) > 10) { //  || this.gesture.duration > 500
+    if (this.trackGesture || this._isTrackeableGesture()) {
       this.trackGesture = true;
+      this.gesture.last.preventDefault();
       requestAnimationFrame(this.updateProgress.bind(this));
     } else {
-      console.log('Gesture too short: ', this.gesture.deltaX);
+      // console.log('Gesture not trackeable: ', this.gesture.deltaX);
     }
   },
 
   updateProgress: function() {
-    var progress = Math.min(Math.max(this.gesture.deltaX / (this.width * 0.75), -1), 1);
-    var scale = this._calculateScale(progress)
-    var translateX = this._calculateTranslate(progress);
-    var transform = 'scale('+scale+') translateX('+translateX+'px)';
+    this.set('progress', -Math.min(Math.max(this.gesture.deltaX / (this.width * 0.75), -1), 1));
+    this._updateStyles();
+  },
 
-    this.set('progress', progress);
-    for (var i = 0, l = this.articles.length; i < l; i++) {
-      this.articles[i].style.transform = transform;
+  finishAnimation: function() {
+    var progress = this.get('progress');
+    var self = this;
+    var speedX, targetValue, delta, min, max;
+
+    if (progress === 0 || progress === 1 || progress === -1) {
+      return;
     }
+
+    speedX = this.gesture.speedX;
+
+    if (speedX < -500) {
+      targetValue = 1; // Go to previous
+      min = 0;
+      max = targetValue;
+    } else if (speedX > 500) {
+      targetValue = -1;  // Go to next
+      min = targetValue;
+      max = 0;
+    } else {
+      targetValue = Math.round(progress); // Go to closest value
+      max = Math.max(targetValue, progress);
+      min = Math.min(targetValue, progress);
+    }
+    delta = Math.sign(targetValue - progress) * this.defaultSpeed;
+
+    function iterate() {
+      var newProgress = Math.min(max, Math.max(min, self.get('progress') + delta));
+      self.set('progress', newProgress);
+      self._updateStyles();
+      if (newProgress !== targetValue) {
+        requestAnimationFrame(iterate);
+      }
+    }
+
+    iterate();
   },
 
   _calculateScale: function(progress){
@@ -67,6 +100,27 @@ export default Ember.View.extend({
   },
 
   _calculateTranslate: function(progress){
-    return Math.sign(progress) * Math.max(Math.min(Math.abs(progress) * (1 / 0.8) - (1 / 0.8) / 10, 1), 0) * this.width;
+    return Math.sign(-progress) * Math.max(Math.min(Math.abs(progress) * (1 / 0.8) - (1 / 0.8) / 10, 1), 0) * this.width;
+  },
+
+  _isTrackeableGesture: function() {
+    var direction;
+    if (Math.abs(this.gesture.deltaX) < 15) {
+      return false;
+    }
+
+    direction = this.gesture.direction;
+    return (direction > 70 && direction < 110) || (direction > 250 && direction < 290);
+  },
+
+  _updateStyles: function(){
+    var progress = this.get('progress');
+    var scale = this._calculateScale(progress);
+    var translateX = this._calculateTranslate(progress);
+    var transform = 'scale('+scale+') translateX('+translateX+'px)';
+
+    for (var i = 0, l = this.articles.length; i < l; i++) {
+      this.articles[i].style.transform = transform;
+    }
   }
 });
