7f692f82b850f85a7f885f28530b861cd24a77a7
Miguel Camba <miguel.camba@gmail.com>
2015-01-22 00:39:21 +0000

    Animations works!

--- app/components/animated-deck.js
+++ app/components/animated-deck.js
@@ -5,7 +5,7 @@ var computed = Ember.computed;
 
 export default Ember.Component.extend({
   classNames: ['animated-deck'],
-  classNameBindings: ['animationClass'],
+  classNameBindings: ['effectClass'],
   duration: 1000,
 
   // Initializer
@@ -13,27 +13,32 @@ export default Ember.Component.extend({
     this.width = this.element.offsetWidth;
     var cards = this.element.querySelectorAll('.animated-card');
     var opts = { duration: this.duration, fill: 'both' };
-    var keyframes = [
-      { transform: `scale(1) translate(-${this.width}px)` },
-      { transform: `scale(0.9) translate(-${this.width}px)` },
-      { transform: `scale(0.9) translate(0px)` },
+    var previousKeyframes = [
       { transform: `scale(1) translate(0px)` },
       { transform: `scale(0.9) translate(0px)` },
       { transform: `scale(0.9) translate(${this.width}px)` },
       { transform: `scale(1) translate(${this.width}px)` }
     ]
-    var animations = [];
+    var nextKeyframes = [
+      { transform: `scale(1) translate(0px)` },
+      { transform: `scale(0.9) translate(0px)` },
+      { transform: `scale(0.9) translate(-${this.width}px)` },
+      { transform: `scale(1) translate(-${this.width}px)` }
+    ]
+    var nextAnimations = [];
+    var previousAnimations = [];
     for (let i = 0; i < cards.length; i++) {
-      animations.push(new Animation(cards[i], keyframes, opts));
+      nextAnimations.push(new Animation(cards[i], nextKeyframes, opts));
+      previousAnimations.push(new Animation(cards[i], previousKeyframes, opts));
     }
-    this.player = document.timeline.play(new AnimationGroup(animations));
-    this.player.pause();
-    this.player.currentTime = this.duration / 2;
+    this.nextAnimation = new AnimationGroup(nextAnimations);
+    this.previousAnimation = new AnimationGroup(previousAnimations);
+    window.debugComponent = this;
   }.on('didInsertElement'),
 
   // CPs
-  animationClass: computed('animation', function() {
-    return `effect-${this.animation}`;
+  effectClass: computed('effect', function() {
+    return `effect-${this.effect}`;
   }),
 
   previous: computed('items.[]', 'current', function() {
@@ -50,35 +55,56 @@ export default Ember.Component.extend({
 
   // Observers
   resetAnimation: function() {
-    this.player.currentTime = this.duration / 2;
+    this.player.currentTime = 0;
+    this.player = null;
   }.observes('current'),
 
   // Event handling
   touchStart: function(e) {
     this.gesture = new Gesture(e.originalEvent);
-    this.gesture.adquire();
   },
   touchMove: function(e) {
     this.gesture.push(e.originalEvent);
-    var progress = (this.gesture.deltaX + 320) / (320 * 2);
-    this.player.currentTime = progress * this.duration;
+    if (this.gesture.isHorizontal()) {
+      this.gesture.adquire();
+      this.player = this.getPlayer();
+      this.player.currentTime = Math.abs(this.gesture.deltaX) / 320 * this.duration;
+    }
   },
   touchEnd: function(e) {
     e.preventDefault();
+    this.finalizeAnimation();
+    this.gesture = null;
+  },
+
+  // Functions
+  getPlayer: function() {
+    if (this.player) {
+      return this.player;
+    }
+    var player;
+    if (this.gesture.deltaX > 0) {
+      player = document.timeline.play(this.previousAnimation);
+    } else {
+      player = document.timeline.play(this.nextAnimation);
+    }
+    player.pause();
+    return player;
+  },
+
+  finalizeAnimation: function() {
     if (this.gesture.speedX > 0) {
-      this.player.playbackRate = 1;
-      this.player.onfinish = (e) => {
+      this.player.onfinish = e => {
         this.player.playbackRate = 0;
         this.sendAction('onChange', this.get('previous'));
       }
     } else if (this.gesture.speedX < 0) {
-      this.player.playbackRate = -1;
-      this.player.onfinish = (e) => {
+      this.player.onfinish = e => {
         this.player.playbackRate = 0;
         this.sendAction('onChange', this.get('next'));
       }
     }
+    this.player.playbackRate = 1;
     this.player.play();
-    this.gesture = null;
   }
 });
