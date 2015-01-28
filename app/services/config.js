d4c0046887ce42162de40ad45227c80eadb7e031
Miguel Camba <miguel.camba@gmail.com>
2015-01-28 00:52:44 +0000

    Check for localstorage presence

--- app/services/config.js
+++ app/services/config.js
@@ -4,10 +4,10 @@ export default Ember.Object.extend({
   deckEffects: ['expose', 'stack'],
   deckEffect: Ember.computed({
     get: function() {
-      return localStorage.deckEffect || this.deckEffects[0];
+      return (localStorage && localStorage.deckEffect) || this.deckEffects[0];
     },
     set: function(key, value) {
-      localStorage.deckEffect = value;
+      localStorage && localStorage.deckEffect = value;
       return value;
     }
   }),
