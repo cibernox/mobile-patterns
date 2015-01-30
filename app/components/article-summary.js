71fd9c8566f39fb055b5b00a448a623e4bae9a1d
Miguel Camba <miguel.camba@gmail.com>
2015-01-30 16:51:10 +0000

    Swipe is cool but click is a better UX

--- app/components/article-summary.js
+++ app/components/article-summary.js
@@ -4,39 +4,9 @@ import Gesture from 'mobile-patterns/utils/gesture';
 export default Ember.Component.extend({
   classNames: ['article-summary'],
 
-  // Events
-  touchStart: function(e) {
-    this.gesture = new Gesture(e.originalEvent);
-  },
-
-  touchMove: function(e) {
-    this.gesture.push(e.originalEvent);
-    if (this.mustTrack()) {
-      this.gesture.adquire();
-      this.sendAction('swipe', this.gesture);
-    }
-  },
-
-  touchEnd: function() {
-    if (this.track) {
-      this.sendAction('release', this.gesture);
-    }
-    this.gesture = null;
-    this.track = undefined;
-    this.neverTrackAgain = false;
-  },
-
-  // Functions
-  mustTrack: function() {
-    if (this.track === undefined && this.gesture.delta > 15) {
-      this.track = !this.neverTrackAgain && this.gesture.isHorizontal(25);
-      if (!this.track) {
-        this.neverTrackAgain = true;
-      } else {
-        this.gesture.initialOffset = this.gesture.deltaX;
-        this.sendAction('select', this.get('article'));
-      }
-    }
-    return !!this.track;
-  },
+  click: function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.sendAction('select', this.get('article'));
+  }
 });
--- app/components/swipeable-list.js
+++ app/components/swipeable-list.js
@@ -2,14 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   duration: 400,
-  inverseEasing: new BezierEasing(0, 0.42, 1, 0.58),
 
   // Callbacks
   setupAnimation: function() {
     this.width = this.element.offsetWidth;
     var articlePreview = this.element.querySelector('.article-detail');
     var keyframes = [{ transform: 'translateX(0)' }, { transform: `translateX(-${this.width}px)` }]
-    var opts = { duration: this.duration, fill: 'both', easing: 'cubic-bezier(0.42, 0, 0.58, 1)' };
+    var opts = { duration: this.duration, fill: 'both', easing: 'ease-in-out' };
     var animation = new Animation(articlePreview, keyframes, opts);
     this.player = document.timeline.play(animation);
     this.player.pause();
@@ -23,25 +22,10 @@ export default Ember.Component.extend({
   actions: {
     select: function(article) {
       this.set('selection', article);
-    },
-
-    animate: function(gesture) {
-      var progress = (-gesture.deltaX + gesture.initialOffset) / this.width;
-      this.player.currentTime = this.inverseEasing(progress) * this.duration;
-    },
-
-    finalize: function(gesture) {
-      var progress = (-gesture.deltaX + gesture.initialOffset) / this.width;
-      var speed = -gesture.speedX / this.width;
-
-      if (speed > 1 || progress > 0.5) {
-        this.player.playbackRate = Math.max(1, speed);
-        this.player.onfinish = () => this.sendAction("select", this.get('selection'));
-      } else {
-        this.player.playbackRate = -1;
-        this.player.onfinish = () => this.player.pause();
-      }
-      this.player.play();
+      Ember.run.schedule('afterRender', () => {
+        this.player.onfinish = () => this.sendAction("select", article);
+        this.player.play();
+      });
     }
   }
 });
