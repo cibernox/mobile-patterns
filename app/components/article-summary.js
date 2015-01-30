720ddd5fd2c2879e9eeeba9512dadd3eda5be260
Miguel Camba <miguel.camba@gmail.com>
2015-01-30 01:42:12 +0000

    More or less I have the idea

---
+++ app/components/article-summary.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+import Gesture from 'mobile-patterns/utils/gesture';
+
+export default Ember.Component.extend({
+  classNames: ['article-summary'],
+
+  // Events
+  touchStart: function(e) {
+    this.gesture = new Gesture(e.originalEvent);
+  },
+
+  touchMove: function(e) {
+    this.gesture.push(e.originalEvent);
+    if (this.mustTrack()) {
+      this.gesture.adquire();
+      this.sendAction('swipe', this.gesture);
+    }
+  },
+
+  touchEnd: function() {
+    if (this.track) {
+      this.sendAction('finalize', this.gesture);
+    }
+    this.gesture = null;
+    this.track = undefined;
+    this.neverTrackAgain = false;
+  },
+
+  // Functions
+  mustTrack: function() {
+    if (this.track === undefined && this.gesture.delta > 15) {
+      this.track = !this.neverTrackAgain && this.gesture.isHorizontal(25);
+      if (!this.track) {
+        this.neverTrackAgain = true;
+      } else {
+        this.initialOffset = this.gesture.deltaX;
+        this.sendAction('select', this.get('article'));
+      }
+    }
+    return !!this.track;
+  },
+});
---
+++ app/components/swipeable-list.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  duration: 400,
+  inverseEasing: new BezierEasing(0, 0.42, 1, 0.58),
+
+  // Callbacks
+  setupAnimation: function() {
+    this.width = this.element.offsetWidth;
+    var articlePreview = this.element.querySelector('.article-detail');
+    var keyframes = [{ transform: 'translateX(0)' }, { transform: `translateX(-${this.width}px)` }]
+    var opts = { duration: this.duration, fill: 'both', easing: 'cubic-bezier(0.42, 0, 0.58, 1)' };
+    var animation = new Animation(articlePreview, keyframes, opts);
+    this.player = document.timeline.play(animation);
+    this.player.pause();
+  }.on('didInsertElement'),
+
+  cleanup: function() {
+    this.set('selection', null);
+  }.on('willDestroy'),
+
+  // Actions
+  actions: {
+    select: function(article) {
+      this.set('selection', article);
+    },
+
+    animate: function(gesture) {
+      var progress = (-gesture.deltaX + this.initialOffset) / this.width;
+      this.player.currentTime = this.inverseEasing(progress) * this.duration;
+    },
+
+    finalize: function(gesture) {
+      var progress = (-gesture.deltaX + this.initialOffset) / this.width;
+      var speed = -this.gesture.speedX / this.width;
+
+      if (speed > 1 || progress > 0.5) {
+        this.player.playbackRate = Math.max(1, speed);
+        this.player.onfinish = () => {
+          var article = this.get('selection');
+          this.get('controller').transitionToRoute('news.show', article);
+        }
+      } else {
+        this.player.playbackRate = -1;
+        this.player.onfinish = () => this.player.pause();
+      }
+      this.player.play();
+    }
+  }
+});
